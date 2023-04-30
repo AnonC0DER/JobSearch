e9b74299d5e9114f4ae9b3755cf18a3e2cd162f4
Hesam Norin <hesam.norin@yahoo.com>
2023-05-01 00:44:50 +0330

    Delete everything

--- Frontend/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from Frontend import views
-
-urlpatterns = [
-    path('', views.SearchPageView, name='search-page'),
-    path('login/', views.LoginViwe, name='login'),
-    path('register/', views.RegisterViwe, name='register'),
-]
